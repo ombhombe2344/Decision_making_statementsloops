3dc7deb7295cf2ceaedc002683262d425204cb6c
ombhombe2344 <ombhombe15@gmail.com>
2024-08-07 15:36:04 +0530

    Update flyodepattern.cpp

--- flyodepattern.cpp
+++ flyodepattern.cpp
@@ -1,3 +1,5 @@
+//23070123039
+// om bhombe
 #include <iostream>
 using namespace std;
 
@@ -22,3 +24,9 @@ int main() {
 
     return 0;
 }
+/*Enter number of rows: 5
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 */

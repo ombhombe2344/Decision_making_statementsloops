b2e2c1e3fa34029648a1aa0f405499878244108e
ombhombe2344 <ombhombe15@gmail.com>
2024-08-07 15:32:59 +0530

    Create flyodepattern.cpp

---
+++ flyodepattern.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int rows;
+    int k=1;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for(int i = 1; i <= rows ; ++i)
+    {
+        for(int j = 1; j <= i ; ++j)
+        {
+            cout << k<<' ';
+            k++ ;
+            
+        }
+        cout << endl;
+    }
+
+    return 0;
+}

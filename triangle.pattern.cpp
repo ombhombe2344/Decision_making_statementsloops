d16b9664005fc969e2ff984ca2f1728b89ab4d58
ombhombe2344 <ombhombe15@gmail.com>
2024-08-09 11:30:13 +0530

    Create triangle.pattern.cpp

---
+++ triangle.pattern.cpp
@@ -0,0 +1,35 @@
+//Name:Om Bhombe
+//PRN:23070123039
+//Aim:Implemention of Decision Making Statements Loops to print Triangle Pattern
+
+#include <iostream>
+using namespace std;
+
+int main(){
+   
+    int n;
+
+    cout<<"Enter The Value for n: ";
+    cin>>n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        for (int k = 1; k <= i; k++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+/*Output:
+Enter The Value for n: 5
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+*/
